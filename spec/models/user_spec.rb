3b43534c977cd0b29a9c8c83a5688f2111c13289
Graham Thompson <grwthomps@gmail.com>
2019-12-19 08:49:25 -0700

    Remove validations from user
    Co-Authored-By: Hillary <hillstew@users.noreply.github.com>
    Co-Authored-By: Laura Schulz <lrs8810@gmail.com>
    Co-Authored-By: Zac Isaacson <zacisaacson@users.noreply.github.com>

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,15 +2,12 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   describe 'validations' do
-    it { should validate_presence_of(:first_name) }
-    it { should validate_presence_of(:last_name) }
-    it { should validate_presence_of(:email) }
-    it { should validate_presence_of(:budget_id) }
-    it { should validate_presence_of(:token) }
+    it { should validate_presence_of(:access_token) }
+    it { should validate_presence_of(:refresh_token) }
     it { should validate_presence_of(:last_login) }
     it { should validate_presence_of(:reminders?) }
     it { should validate_uniqueness_of(:email) }
-  end 
+  end
 
   describe 'relationships' do
     it { should have_many(:accounts) }
