2af70e43a291d249d03a8d09f8b3e2d284217f03
Hillary Stewart <hillarystewart4427@gmail.com>
2019-12-18 20:04:33 -0700

    Add SessionsController with create action
    Co-Authored-By: Laura Schulz <lrs8810@gmail.com>
    Co-Authored-By: Zac Isaacson <zacisaacson@users.noreply.github.com>
    Co-Authored-By: Graham Thompson <grwthomps@gmail.com>

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,8 @@
+class SessionsController < ApplicationController
+  def create
+    code = params["code"]
+    response = Faraday.post("https://app.youneedabudget.com/oauth/token?client_id=#{ENV['CLIENT_ID']}&client_secret=#{ENV['CLIENT_SECRET']}&redirect_uri=http://localhost:3000/auth/ynab/callback&grant_type=authorization_code&code=#{code}")
+
+    
+  end
+end
