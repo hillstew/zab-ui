78604849d954b60bc688d3308ded12ee7ae0960f
Laura Schulz <lrs8810@gmail.com>
2019-12-19 18:06:31 -0700

    Add account instance methods
    Co-Authored-By: Graham Thompson <grwthomps@gmail.com>
    Co-Authored-By: Hillary <hillstew@users.noreply.github.com>
    Co-Authored-By: Zac Isaacson <zacisaacson@users.noreply.github.com>

--- app/models/account.rb
+++ app/models/account.rb
@@ -6,5 +6,25 @@ class Account < ApplicationRecord
   validates_presence_of :starting_total
   validates_presence_of :interest_rate
   validates_presence_of :min_payment
-  validates_presence_of :paid_off?
+
+  def formatted_balance
+    starting_total = self.starting_total / 1000
+  end
+
+  def total_months
+    count = 0
+    balance = self.balance
+    while balance > 0
+      # min_pay = self.balance  * (self.interest_rate / 100)
+      interest_paid = self.min_payment * (self.interest_rate / 100 )
+      balance = balance - (self.min_payment - interest_paid)
+      count += 1
+    end
+    payoff_month(count)
+    # binding.pry
+  end
+
+  def payoff_month(count)
+    (DateTime.now + count.months).strftime("%b %Y")
+  end
 end
