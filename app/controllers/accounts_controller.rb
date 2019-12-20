52bbfa4f58d6dad3fbf0b37c8a295561ec517686
Laura Schulz <lrs8810@gmail.com>
2019-12-19 18:04:59 -0700

    Add create action and account_params
    Co-Authored-By: Graham Thompson <grwthomps@gmail.com>
    Co-Authored-By: Hillary <hillstew@users.noreply.github.com>
    Co-Authored-By: Zac Isaacson <zacisaacson@users.noreply.github.com>

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -14,6 +14,27 @@ class AccountsController < ApplicationController
   end
 
   def create
-    binding.pry
+    params['accounts'].each do |account, details|
+      if details.has_key?('checked')
+        params = account_params(account, details)
+        account = Account.create!(params)
+      end
+    end
+    redirect_to dashboard_path
   end
+
+  private
+  def account_params(account, details)
+    {
+      name: account,
+      user_id: current_user.id,
+      ynab_id: details['ynab_id'],
+      min_payment: details['min_payment'].to_f,
+      starting_total: details['starting_total'].to_f.abs / 1000,
+      interest_rate: details['interest_rate'].to_f,
+      balance: (details['starting_total'].to_f.abs / 1000)
+    }
+  end
+
+
 end
