e37624a06c89aaad3c8809de5939426dee5aab00
Zac Isaacson <isaacson.zm@gmail.com>
2019-12-30 14:59:42 -0700

    Create Ynab service and refactor
    Co-Authored-By: Hillary <hillstew@users.noreply.github.com>
    Co-Authored-By: Graham Thompson <grwthomps@gmail.com>
    Co-Authored-By: Laura Schulz <lrs8810@gmail.com>

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,17 +4,16 @@ class UsersController < ApplicationController
   end
 
   def create
-    code = params["code"]
-
-    response = Faraday.post("https://app.youneedabudget.com/oauth/token?client_id=#{ENV['CLIENT_ID']}&client_secret=#{ENV['CLIENT_SECRET']}&redirect_uri=http://localhost:3000/auth/ynab/callback&grant_type=authorization_code&code=#{code}")
-    raw_token_data = JSON.parse(response.body, symbolize_names: true)
-    user = User.create!(access_token: raw_token_data[:access_token], refresh_token: raw_token_data[:refresh_token], last_login: DateTime.now)
+    tokens = YnabService.new.fetch_tokens(params['code'])
+    user_data = format_user_data(tokens)
+    binding.pry
+    user = User.create!(user_data)
     session[:user_id] = user.id
     redirect_to signup_profile_path
   end
 
   def update
-    user = current_user.update!(user_params)
+    current_user.update!(user_params)
     redirect_to signup_accounts_path
   end
 
@@ -23,6 +22,14 @@ class UsersController < ApplicationController
   end
 
   private
+
+  def format_user_data(raw_token_data)
+    {
+      access_token: raw_token_data[:access_token],
+      refresh_token: raw_token_data[:refresh_token],
+      last_login: DateTime.now
+    }
+  end
 
   def user_params
     params.require(:user).permit(:first_name, :last_name, :email)
--- app/services/ynab_service.rb
+++ app/services/ynab_service.rb
@@ -1,5 +1,24 @@
 class YnabService
-  def accounts
+  def fetch_accounts
+    response = File.read('./spec/fixtures/accounts_response.json')
+    parse_data(response)
+  end
 
+  def fetch_tokens(code)
+    response = Faraday.post("https://app.youneedabudget.com/oauth/token?client_id=#{ENV['CLIENT_ID']}&client_secret=#{ENV['CLIENT_SECRET']}&redirect_uri=http://localhost:3000/auth/ynab/callback&grant_type=authorization_code&code=#{code}")
+    parse_data(response.body)
+  end
+
+private
+
+  def connection
+    Faraday.new("https://api.youneedabudget.com/v1/budgets/default") do |faraday|
+      faraday.params["access_token"] = current_user.access_token
+      faraday.adapter Faraday.default_adapter
+    end
+  end
+
+  def parse_data(response)
+    JSON.parse(response, symbolize_names: true)
   end
 end
