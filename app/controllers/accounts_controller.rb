4e24ec43914808472e0fcab7f8cad85082dcec1d
Hillary Stewart <hillarystewart4427@gmail.com>
2020-01-03 13:14:28 -0700

    Remove extra lines

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -13,24 +13,22 @@ class AccountsController < ApplicationController
   end
 
   private
-  def account_params(account, details)
-    {
-      name: account,
-      user_id: current_user.id,
-      ynab_id: details['ynab_id'],
-      min_payment: details['min_payment'].to_f,
-      starting_total: details['starting_total'].to_f.abs / 1000,
-      interest_rate: details['interest_rate'].to_f,
-      balance: (details['starting_total'].to_f.abs / 1000)
-    }
-  end
-
-  def check_key(account, details)
-    if details.has_key?('checked')
-      params = account_params(account, details)
-      account = Account.create!(params)
+    def account_params(account, details)
+      {
+        name: account,
+        user_id: current_user.id,
+        ynab_id: details['ynab_id'],
+        min_payment: details['min_payment'].to_f,
+        starting_total: details['starting_total'].to_f.abs / 1000,
+        interest_rate: details['interest_rate'].to_f,
+        balance: (details['starting_total'].to_f.abs / 1000)
+      }
     end
-  end
 
-
+    def check_key(account, details)
+      if details.has_key?('checked')
+        params = account_params(account, details)
+        account = Account.create!(params)
+      end
+    end
 end
