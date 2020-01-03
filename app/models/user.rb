8fb26f621a60d6487e97dc62760fd2837b9b77d2
Graham Thompson <grwthomps@gmail.com>
2020-01-03 10:56:12 -0700

    Refactor user model validations
    Co-Authored-By: Hillary <hillstew@users.noreply.github.com>
    Co-Authored-By: Laura Schulz <lrs8810@gmail.com>
    Co-Authored-By: Zac Isaacson <zacisaacson@users.noreply.github.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,11 @@ class User < ApplicationRecord
   has_many :connections
   has_many :connected_users, through: :connections
 
-  validates_presence_of :access_token
-  validates_presence_of :refresh_token
-  # validates_presence_of :reminders?
-  validates_presence_of :last_login
+  # validates_presence_of :access_token
+  # validates_presence_of :refresh_token
+  # validates :reminders?
+  # validates_presence_of :last_login
+  validates_presence_of :google_token
   validates_presence_of :monthly_payment
-  # validates :email, uniqueness: true
+  validates :email, uniqueness: true
 end
