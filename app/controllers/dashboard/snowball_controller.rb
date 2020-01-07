fd13772b2c59c4cf4de26722142e8e0224219d68
Hillary Stewart <hillarystewart4427@gmail.com>
2020-01-06 17:09:21 -0700

    Update current_user with snowball_amount
    Co-Authored-By: Laura Schulz <lrs8810@gmail.com>
    Co-Authored-By: Graham Thompson <grwthomps@gmail.com>
    Co-Authored-By: Zac Isaacson <zacisaacson@users.noreply.github.com>

--- app/controllers/dashboard/snowball_controller.rb
+++ app/controllers/dashboard/snowball_controller.rb
@@ -1,5 +1,6 @@
 class Dashboard::SnowballController < ApplicationController
   def index
+    current_user.update(snowball_amount: params[:amount])
     response = current_user.accounts.snowball(params[:amount])
     render json: response
   end
