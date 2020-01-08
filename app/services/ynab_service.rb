ca7d9a8c79088f1e71f62a62f96428cf0e0cef70
Laura Schulz <lrs8810@gmail.com>
2020-01-08 09:55:02 -0700

    Refactor domains to be dynamic using environment variables
    Co-Authored-By: Hillary <hillstew@users.noreply.github.com>

--- app/services/ynab_service.rb
+++ app/services/ynab_service.rb
@@ -4,7 +4,7 @@ class YnabService
   end
 
   def fetch_tokens(code)
-    response = Faraday.post("https://app.youneedabudget.com/oauth/token?client_id=#{ENV['CLIENT_ID']}&client_secret=#{ENV['CLIENT_SECRET']}&redirect_uri=https://zeroandbeyond.herokuapp.com/auth/ynab/callback&grant_type=authorization_code&code=#{code}")
+    response = Faraday.post("https://app.youneedabudget.com/oauth/token?client_id=#{ENV['CLIENT_ID']}&client_secret=#{ENV['CLIENT_SECRET']}&redirect_uri=#{ENV['REDIRECT_DOMAIN']}/auth/ynab/callback&grant_type=authorization_code&code=#{code}")
     parse_data(response.body)
   end
 
