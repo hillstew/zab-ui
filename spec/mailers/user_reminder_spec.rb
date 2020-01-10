b05e5b21a1c96fd8b297e9952941d03b3df37973
Laura Schulz <lrs8810@gmail.com>
2020-01-09 18:08:38 -0700

    Update specs
    Co-Authored-By: Graham Thompson <grwthomps@gmail.com>
    Co-Authored-By: Zac Isaacson <isaacson.zm@gmail.com>
    Co-Authored-By: Hillary Stewart <hillarystewart4427@gmail.com>

--- spec/mailers/user_reminder_spec.rb
+++ spec/mailers/user_reminder_spec.rb
@@ -1,5 +1,12 @@
 require "rails_helper"
 
 RSpec.describe UserReminderMailer, type: :mailer do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'sends reminder emails to users' do
+    user = create(:user, first_name: 'Bob', last_name: 'G', email: 'bob@gmail.com')
+    mail = UserReminderMailer.send_reminder(user)
+
+    expect(mail.subject).to eq("#{user.first_name}, a quick reminder from ZAB")
+    expect(mail.to).to eq([user.email])
+    expect(mail.from).to eq(['no-reply@zeroandbeyond.herokuapp.com'])
+  end
 end
