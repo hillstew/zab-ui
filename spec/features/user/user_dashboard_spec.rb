36137d70a6d8c9490d47417fe277d987fc6533dd
Laura Schulz <lrs8810@gmail.com>
2020-01-09 18:07:36 -0700

    Add Dashboard::SnowballController create action
    Co-Authored-By: Graham Thompson <grwthomps@gmail.com>
    Co-Authored-By: Zac Isaacson <isaacson.zm@gmail.com>
    Co-Authored-By: Hillary Stewart <hillarystewart4427@gmail.com>

--- spec/features/user/user_dashboard_spec.rb
+++ spec/features/user/user_dashboard_spec.rb
@@ -1,29 +1,16 @@
 require 'rails_helper'
 
-RSpec.describe 'As a logged in user' do
-  xit "snowball amount persists on page reload" do
-    user = create(:user)
-    account_1 = create(:account, balance: 2345, interest_rate: 10, min_payment: 200, user_id: user.id)
-    account_2 = create(:account, balance: 3409, interest_rate: 10, min_payment: 100, user_id: user.id)
-    account_3 = create(:account, balance: 12323, interest_rate: 10, min_payment: 150, user_id: user.id)
+RSpec.describe Dashboard::SnowballController, :type => :request do
+  describe "GET #new " do
+    it "snowball amount persists on page reload" do
+      user = create(:user)
+      create(:account, balance: 2345, interest_rate: 10, min_payment: 200, user_id: user.id)
 
-    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
-    visit dashboard_path
+      get "#{ENV['REDIRECT_DOMAIN']}/snowball/1000"
 
-    expect(page).to have_css('#snowball-input')
-
-    fill_in 'snowball', with: 1000
-
-    click_button 'Calculate'
-
-    visit dashboard_path
-
-    account_1.reload
-
-    expect(account_1.snowball).to eq(550)
-    # within "#account-#{account_1.id}" do
-    #   expect(page).to have_content(550)
-    # end
+      expect(response).to have_http_status(:success)
+    end
   end
 end
