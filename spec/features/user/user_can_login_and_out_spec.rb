ce4f21c70f8040e5059bd01155094e24b24b27c6
Laura Schulz <lrs8810@gmail.com>
2020-01-10 08:25:58 -0700

    Add tests for finding a returing user and a user that needs to add YNAB accounts
    Co-Authored-By: Zac Isaacson <zacisaacson@users.noreply.github.com>

--- spec/features/user/user_can_login_and_out_spec.rb
+++ spec/features/user/user_can_login_and_out_spec.rb
@@ -2,10 +2,11 @@ require 'rails_helper'
 
 RSpec.describe 'As a user' do
   it 'they can log in' do
-    user = create(:user)
-    account_1 = create(:account, balance: 2345, interest_rate: 10, min_payment: 200, user_id: user.id)
-    
-    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+    user_1 = create(:user, email:"gthompson@fastmail.com", first_name: "Graham", last_name: "Thompson", google_token: ENV["GOOGLE_TEST_TOKEN"])
+    create(:account, balance: 2345, interest_rate: 10, min_payment: 200, user_id: user_1.id)
+
+
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user_1)
 
     visit welcome_path
 
@@ -14,13 +15,39 @@ RSpec.describe 'As a user' do
     expect(current_path).to eq(dashboard_path)
   end
 
-  it 'they can log out' do
-    user = create(:user)
-    account_1 = create(:account, balance: 2345, interest_rate: 10, min_payment: 200, user_id: user.id)
-    account_2 = create(:account, balance: 3409, interest_rate: 10, min_payment: 100, user_id: user.id)
-    account_3 = create(:account, balance: 12323, interest_rate: 10, min_payment: 150, user_id: user.id)
+  it 'they are directed to signup accounts page if they do not have YNAB accounts' do
+    create(:user)
 
-    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+    visit welcome_path
+
+    click_button 'Login with Google'
+
+    visit welcome_path
+
+    click_button 'Login with Google'
+
+    expect(current_path).to eq(signup_ynab_path)
+  end
+
+  it 'they are redirected to dashboard page ' do
+    user_1 = create(:user, email:"gthompson@fastmail.com", first_name: "Graham", last_name: "Thompson", google_token: ENV["GOOGLE_TEST_TOKEN"])
+    create(:account, balance: 2345, interest_rate: 10, min_payment: 200, user_id: user_1.id)
+    
+    visit welcome_path
+
+    click_button 'Login with Google'
+
+    expect(current_path).to eq(dashboard_path)
+  end
+
+  it 'they can log out' do
+    user_1 = create(:user, email:"gthompson@fastmail.com", first_name: "Graham", last_name: "Thompson", google_token: ENV["GOOGLE_TEST_TOKEN"])
+
+    create(:account, balance: 2345, interest_rate: 10, min_payment: 200, user_id: user_1.id)
+    create(:account, balance: 3409, interest_rate: 10, min_payment: 100, user_id: user_1.id)
+    create(:account, balance: 12323, interest_rate: 10, min_payment: 150, user_id: user_1.id)
+
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user_1)
 
     visit dashboard_path
 
