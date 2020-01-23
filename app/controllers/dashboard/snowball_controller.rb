e72a1bc28efeb6af686258cd24db0b97c9c51440
Graham Thompson <grwthomps@gmail.com>
2020-01-23 14:31:26 -0700

    Refactor snowball and dashboard controllers to use new debt_accounts method
    Co-Authored-By: Hillary <hillstew@users.noreply.github.com>
    Co-Authored-By: Laura Schulz <lrs8810@gmail.com>
    Co-Authored-By: Zac Isaacson <zacisaacson@users.noreply.github.com>

--- app/controllers/dashboard/snowball_controller.rb
+++ app/controllers/dashboard/snowball_controller.rb
@@ -1,7 +1,7 @@
 class Dashboard::SnowballController < ApplicationController
   def index
     current_user.update(snowball_amount: params[:amount])
-    response = current_user.accounts.snowball(params[:amount])
+    response = current_user.accounts.debt_accounts.snowball(params[:amount])
     render json: response
   end
 end
--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,8 +2,8 @@ class DashboardController < ApplicationController
   def index
     if current_user.snowball_amount == 0
       @accounts = current_user.accounts.ordered
-    else 
-      @accounts = current_user.accounts.snowball(current_user.snowball_amount)
+    else
+      @accounts = current_user.accounts.debt_accounts.snowball(current_user.snowball_amount)
     end
   end
 end
