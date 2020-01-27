f450cd18f2ee69b66d13c5db502ead9e78b03dc1
Zac Isaacson <isaacson.zm@gmail.com>
2020-01-27 14:04:05 -0700

    Add privacy policy route, controller, view and styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/', to: 'welcome#index', as: 'welcome'
+  get '/privacypolicy', to: 'privacy_policy#index'
   get '/auth/ynab/callback', to: 'ynab#create'
   post '/signup', to: 'users#create'
   delete 'logout', to: 'sessions#destroy'
